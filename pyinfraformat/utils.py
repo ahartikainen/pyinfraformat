e26bc570f5b6cb9dd6e635797409185453b1237f
Ari Hartikainen <ari.hartikainen@aalto.fi>
2019-01-19 21:12:05 +0200

    fix typo

--- pyinfraformat/utils.py
+++ pyinfraformat/utils.py
@@ -678,4 +678,4 @@ def print_info(language="fi"):
     if language.lower() != "fi":
         logger.critical("Only 'fi' info is implemented")
         raise NotImplementedError("Only 'fi' info is implemented")
-    print(_info_fi())
+    print(info_fi())
