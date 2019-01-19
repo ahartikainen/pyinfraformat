d0ed5fbc65318f88bc83e4de6c7f4409c9477983
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2019-01-19 14:28:57 +0200

    Update test_general.py

--- pyinfraformat/tests/test_general.py
+++ pyinfraformat/tests/test_general.py
@@ -4,12 +4,12 @@ import pytest
 
 @pytest.mark.parametrize("language", ['fi', 'Fi', 'FI', 'fI'])
 def test_info(language):
-    assert print_info() is None
-    assert print_info(language=language) is None
-    assert isinstance(_info_fi(), str) 
+    assert pif.print_info() is None
+    assert pif.print_info(language=language) is None
+    assert isinstance(pif._info_fi(), str) 
 
 
 @pytest.mark.parametrize("language", ['se', 'en', 'fin'])
 def test_info_bad(language):
     with pytest.raises(NotImplementedError):
-        print_info(language)
+        pif.print_info(language)
