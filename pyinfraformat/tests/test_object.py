6341c4ef1431d0b7088c0c327abd44e1f58d18ac
mhallipelto <martti.hallipelto@outlook.com>
2020-02-06 22:57:39 +0200

    black

--- pyinfraformat/tests/test_object.py
+++ pyinfraformat/tests/test_object.py
@@ -62,6 +62,7 @@ def test_filter_by_date():
     filtered_holes = holes.filter_holes(start="2014-05-18", end="2019-01-10", fmt="%Y-%m-%d")
     assert len(filtered_holes) <= len(holes)
 
+
 def test_filter_by_type():
     holes = get_object()
     filtered_holes = holes.filter_holes(hole_type=["PO"])
