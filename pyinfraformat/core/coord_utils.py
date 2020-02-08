3c167008f8dcfe0b3b3906ce21edcdb848f90714
mhallipelto <martti.hallipelto@outlook.com>
2020-02-08 22:08:38 +0200

    Espoo, Helsinki, Porvoo coordinate transformations

--- pyinfraformat/core/coord_utils.py
+++ pyinfraformat/core/coord_utils.py
@@ -18,6 +18,48 @@ def coord_string_fix(input_string):
     return "-".join(re.split(common_separators, input_string.upper()))
 
 
+def proj_espoo(x, y):
+    """Project Espoo coordinates into ETRS-GK24.
+
+    https://www.espoo.fi/fi-FI/Asuminen_ja_ymparisto/Rakentaminen
+    /Kiinteiston_muodostus/Koordinaattiuudistus(19923)"""
+    # pylint: disable=invalid-name
+    a = 6599858.007479810200000
+    b = 24499824.978235636000000
+    c = 0.999998786628487
+    d = 0.000020762261526
+    e = -0.000014784506306
+    f = 0.999996546603269
+    return a + c * x + d * y, b + e * x + f * y
+
+
+def proj_helsinki(x, y):
+    """Project Helsinki coordinates into ETRS-GK25.
+
+    https://www.hel.fi/helsinki/fi/kartat-ja-liikenne/kartat-ja-paikkatieto
+    /paikkatiedot+ja+-aineistot/koordinaatistot_ja+_korkeudet/koordinaatti_ja_korkeusjarjestelmat"""
+    # pylint: disable=invalid-name
+    a = 6654650.14636
+    b = 25447166.49457
+    c = 0.99998725362
+    d = -0.00120230340
+    e = 0.00120230340
+    f = 0.99998725362
+    return a + c * x + d * y, b + e * x + f * y
+
+
+def proj_porvoo(x, y):
+    """Project Helsinki coordinates into KKJ3.
+
+    https://www.porvoo.fi/koordinaatti-ja-korkeusjarjestelma"""
+    # pylint: disable=invalid-name
+    P = x - 6699461.017
+    I = y - 427129.490
+    x = 6699460.034 + 1.0000817225 * P - 0.0000927982 * I
+    y = 3427132.007 + 1.0000817225 * I + 0.0000927982 * P
+    return x, y
+
+
 def project_hole(hole, output_epsg="EPSG:4326"):
     """Transform holes -objects coordinates.
 
