5e25c89fea392b7bff145a6991b24b20f3c2be35
mhallipelto <martti.hallipelto@outlook.com>
2020-04-05 19:25:12 +0300

    HE plot

--- pyinfraformat/plots/holes.py
+++ pyinfraformat/plots/holes.py
@@ -300,6 +300,64 @@ def plot_tr(one_survey):
 
     return fig
 
+def plot_he(one_survey):
+    """Plot a diagram of HE (Heijarikairaus) with matlplotlib.
+
+    Parameters
+    ----------
+    one_survey : hole object
+
+    Returns
+    -------
+    figure : matlplotlib figure
+    """
+    df = pd.DataFrame(one_survey.survey.data)
+    if "Soil type" in df.columns:  # pylint: disable=unsupported-membership-test
+        soils = df.dropna(subset=["Soil type"])
+    else:
+        soils = None
+    fig, (ax_left, ax_right) = plt.subplots(
+        1, 2, sharey=True, figsize=(4, 4), gridspec_kw={"wspace": 0, "width_ratios": [1, 3]}
+    )
+    fig.set_figwidth(4)
+    ax_left.invert_yaxis()
+    ax_left.spines["top"].set_visible(False)
+    ax_left.spines["left"].set_visible(False)
+    ax_left.get_yaxis().set_visible(False)
+    plt.setp(ax_left.get_yticklabels(), visible=False)
+    ax_left.set_xticks([])
+    ax_right.barh(
+        [0] + list(df["Depth (m)"])[:-1],
+        df["Blows"],
+        align="edge",
+        fill=False,
+        height=df["Depth (m)"].diff(periods=1),
+        linewidth=1.5,
+    )
+    ax_right.yaxis.set_tick_params(which="both", labelbottom=True)
+    ax_right.set_xlim([0, 110])
+    ax_right.set_xticks(list(range(0, 120, 20)))
+    ymax_atleast = 5
+    ymax = ax_right.get_ylim()[0]
+    if ymax < ymax_atleast:
+        ymax = ymax_atleast
+    ax_right.set_ylim(ymax, 0)
+
+    ax_right.spines["top"].set_visible(False)
+    ax_right.spines["right"].set_visible(False)
+
+    ax_right.set_title(one_survey.header.date.isoformat().split("T")[0])
+    ax_left.set_title("{:+.2f}".format(float(one_survey.header["XY"]["Z-start"])))
+
+    last = df["Depth (m)"].iloc[-1]
+    ax_right.plot(0, last, marker="_", zorder=10, clip_on=False, ms=20, c="k")
+    ax_right.text(0.10, last, s=one_survey.header["-1"]["Ending"], va="top")
+    
+    if soils is not None:
+        for _, row in soils.iterrows():
+            ax_right.text(0.03, row["Depth (m)"], s=row["Soil type"])
+            
+    return fig
 
 def plot_hole(one_survey, backend="matplotlib"):
     """Plot a diagram of a sounding with matplotlib.
@@ -328,6 +386,8 @@ def plot_hole(one_survey, backend="matplotlib"):
         fig = plot_si(one_survey)
     elif hole_type == "TR":
         fig = plot_tr(one_survey)
+    elif hole_type == "HE":
+        fig = plot_he(one_survey)
     else:
         raise NotImplementedError('Hole object "{}" not supported'.format(hole_type))
 
