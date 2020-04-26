9f90f6fefe0162e1446bcff371f87e72b15391e5
mhallipelto <martti.hallipelto@outlook.com>
2020-04-26 20:08:47 +0300

    fix tests

--- pyinfraformat/tests/test_io.py
+++ pyinfraformat/tests/test_io.py
@@ -59,7 +59,7 @@ def test_reading_empty():
 
 def test_gtk_wfs():
     bbox = (60.2, 24.8, 60.215, 24.83)
-    holes = pif.from_gtk_wfs(bbox, input_epsg = "EPSG:4326",maxholes= 500)
+    holes = from_gtk_wfs(bbox, input_epsg = "EPSG:4326",maxholes= 500)
     assert isinstance(holes, Holes)
 
 def test_output():
