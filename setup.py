58bffa52d4dfcd441f11d7e52bd7f9ebe14219fb
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2020-06-13 04:34:37 +0300

    Run tests with Github Actions (#44)
    * add actions workflow
    
    * fix workflow
    
    * fix workflow
    
    * fix typo
    
    * move test folder
    
    * use coveralls action
    
    * fix
    
    * use codecov
    
    * install windows wheel"
    
    * install windows wheel"
    
    * codecov yml
    
    * install wheel first
    
    * fix whl
    
    * fix
    
    * upload only from ubuntu
    
    Co-authored-by: ahartikainen <ahartikainen@github.com>

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     keywords="infraformat",
     packages=find_packages(exclude=["docs", "tests"]),
