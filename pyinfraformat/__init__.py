ef44ba4561e01e6f0370761fec840e1d484a6e1f
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2020-10-19 14:59:48 +0300

    Update __init__.py

--- pyinfraformat/__init__.py
+++ pyinfraformat/__init__.py
@@ -17,5 +17,5 @@ if not logging.root.handlers:
     logger.addHandler(handler)
 
 
-__version__ = "20.9.12"
+__version__ = "20.10.19"
 __all__ = ["Holes", "from_infraformat", "to_infraformat", "print_info"]
