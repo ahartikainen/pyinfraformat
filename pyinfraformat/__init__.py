93c1a98c0ec687c901d338597725047337908e39
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2020-10-31 23:45:05 +0200

    Bump version

--- pyinfraformat/__init__.py
+++ pyinfraformat/__init__.py
@@ -17,5 +17,5 @@ if not logging.root.handlers:
     logger.addHandler(handler)
 
 
-__version__ = "20.10.27"
+__version__ = "20.10.31"
 __all__ = ["Holes", "from_infraformat", "to_infraformat", "print_info"]
