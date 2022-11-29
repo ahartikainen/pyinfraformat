8133e8cb08e420f58c416ab65add48df08e061ee
Martti <martti.hallipelto@outlook.com>
2022-11-29 13:42:50 +0200

    update version

--- pyinfraformat/__init__.py
+++ pyinfraformat/__init__.py
@@ -28,5 +28,5 @@ def log_to_file(filename):
     logger.handlers = [file_handler]
 
 
-__version__ = "21.6.16"
+__version__ = "22.11.29"
 __all__ = ["Holes", "from_infraformat", "to_infraformat", "print_info"]
