6d4635a8958671a4c9aa3553d6e59ce69f608191
mhallipelto <44357059+mhallipelto@users.noreply.github.com>
2025-06-04 13:51:53 +0300

    ruff files

--- pyinfraformat/core/coord_utils.py
+++ pyinfraformat/core/coord_utils.py
@@ -47,7 +47,9 @@ def coord_str_recognize(input_string):
     except pyproj.exceptions.CRSError as error:
         error_string = error.args[0]
         if "several objects matching this name" not in error_string:
-            if ("unrecognized format / unknown name" in error_string) and ("ETRS" not in input_string):
+            if ("unrecognized format / unknown name" in error_string) and (
+                "ETRS" not in input_string
+            ):
                 return coord_str_recognize("ETRS " + input_string)
             raise
         error_string = error_string[error_string.rfind(": ") + 2 : -1]
